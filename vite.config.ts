3a4983fd3a01210e99339220cefde6483b62ffb8
Simon <raess.simon@gmail.com>
2026-02-08 17:16:16 +0100

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -13,9 +12,5 @@ export default defineConfig({
         changeOrigin: true,
       }
     }
-  },
-  test: {
-    globals: true,
-    environment: 'node',
   }
 })
